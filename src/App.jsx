177a4e8ac4a010289104f3ba65eeb8efc3197612
Suhan Makhare <suhanmakhare@gmail.com>
2025-08-05 20:50:55 +0530

    Add routing with react-router-dom; create Login and Signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router , Routes , Route , useLocation} from "react-router-dom";
 import "./App.css";
+import React from "react";
 import Navbar from "./components/navbar.jsx";
-function App() {
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
 
+
+function App() {
+  const location = useLocation();
+  const hidenavbar = ["/login", "/signup"].includes(window.location.pathname);
   return (
     <>
-      <Navbar/>
+      {!hidenavbar && <Navbar />}
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const signup = () => {
+
+
+  return (
+    <div >
+      <div>
+        <div>
+            <h2>Signup</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default signup
