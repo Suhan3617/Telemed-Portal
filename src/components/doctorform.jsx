0dcc79b6e440641a7a7e6bf8df1fba188c91995e
Suhan3617 <suhanmakhare@gmail.com>
2025-08-08 16:45:23 +0530

    minor updates in doctorform

--- src/components/doctorform.jsx
+++ src/components/doctorform.jsx
@@ -50,12 +50,12 @@ const doctorform = () => {
         <div>
           <input
             className="w-full bg-white border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 my-2"
-            type="number"
+            type="text"
             placeholder="Years of Experience"
           />
           <input
             className="w-full bg-white border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 my-2"
-            type="number"
+            type="text"
             placeholder="Doctor's Unique Identification Number"
           />
           <input
