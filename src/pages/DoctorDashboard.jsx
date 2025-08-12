2232c27b31affb059e63dc057b4772b96ea6a5dc
Suhan3617 <suhanmakhare@gmail.com>
2025-08-12 23:47:02 +0530

    Add DoctorDashboard component: implement patient list and modal functionality

--- src/pages/DoctorDashboard.jsx
+++ src/pages/DoctorDashboard.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import Doctorsidebar from '../components/doctorsidebar'
+import Doctortopbar from '../components/doctortopbar'
+import Doctorappointmentcard from '../components/doctorappointmentcard'
+import Doctorappointmentdetialsmodal from '../components/doctorappointmentdetialsmodal'
 
 const DoctorDashboard = () => {
+  const [selectedpatient, setselectedpatient] = useState(null);
+  const [isModalOpen, setisModalOpen] = useState(false);
+
+  const patients=[
+    {
+      name:"Jonny Lever",
+      date:"12 Aug 2025",
+      time:"11:00 PM",
+      type:"Video Consultation",
+      history:[
+        "Diabetes-2021" , 
+        "Hypertension-2020"
+      ],
+      labReports: [{ name: "Blood Sugar Test", date: "05 Jan 2025" }]
+    },
+    {
+      name: "Jane Smith",
+      date: "13 Aug 2025",
+      time: "11:00 AM",
+      type: "In-person",
+      history: ["Asthma - 2019"],
+      prescriptions: [],
+      labReports: []
+    }
+  ]
+
+  const openDetails = (patient)=>{
+    setselectedpatient(patient);
+    setisModalOpen(true);
+  }
+
   return (
-    <div>
-      
+    <div className='flex h-screen'>
+      <Doctorsidebar/>
+      <div className='flex flex-col flex-1'>
+        <Doctortopbar/>
+        <div className='p-6 grid gap-5'>
+          {patients.map((p,idx)=>(
+              <Doctorappointmentcard
+              key={idx}
+              patient={p}
+              onViewDetails={openDetails}
+              />
+          ))}
+        </div>
+        <Doctorappointmentdetialsmodal
+        isOpen={isModalOpen}
+        patient={selectedpatient}
+        onClose={()=>setisModalOpen(false)}
+        />
+      </div>
     </div>
   )
 }
