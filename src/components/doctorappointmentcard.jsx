1c9c1004100a2d9f56a02e1dec624ded2cae2cb4
Suhan3617 <suhanmakhare@gmail.com>
2025-08-12 23:23:04 +0530

    Refactor DoctorAppointmentCard and DoctorTopBar components: improve structure and styling

--- src/components/doctorappointmentcard.jsx
+++ src/components/doctorappointmentcard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const doctorappointmentcard = () => {
+const doctorappointmentcard = ({ patient, onViewDetails }) => {
   return (
-    <div>
-      
+    <div className="bg-white shadow rounded-lg p-4 flex justify-between items-center">
+      <div>
+        <h2 className="text-lg font-semibold">{patient.name}</h2>
+        <p className="text-lg font-semibold">
+          {patient.date} | {patient.time}
+        </p>
+      </div>
+      <p className="text-lg font-semibold"> {patient.type}</p>
+      <button
+        onClick={() => onViewDetails(patient)}
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-200"
+      >
+        View Details
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default doctorappointmentcard
+export default doctorappointmentcard;
--- src/components/doctortopbar.jsx
+++ src/components/doctortopbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const doctortopbar = () => {
   return (
-    <div>
-      
+    <div className="bg-white shadow-md p-4 flex justify-between items-center">
+      <h2 className="text-2xl font-semibold">Welcome Back Dr.Smith</h2>
+      <div className="flex items-center gap-4">
+        <input
+          type="text"
+          placeholder="search..."
+          className="border rounded-lg  p-4"
+        />
+        <img
+          src="https://via.placeholder.com/40"
+          alt="Doctor"
+          className="rounded-full w-10 h-10"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default doctortopbar
+export default doctortopbar;
