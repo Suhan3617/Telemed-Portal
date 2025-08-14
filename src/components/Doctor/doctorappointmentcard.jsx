51e450a4182016f3d3552a65977ced6953523653
Suhan3617 <suhanmakhare@gmail.com>
2025-08-14 13:32:09 +0530

    refractor Doctor Appointment Card

--- src/components/Doctor/doctorappointmentcard.jsx
+++ src/components/Doctor/doctorappointmentcard.jsx
@@ -1,21 +1,36 @@
+import { FileText, Video } from "lucide-react";
 import React from "react";
 
-const doctorappointmentcard = ({ patient, onViewDetails }) => {
+const doctorappointmentcard = ({ appt, onViewDetails }) => {
   return (
-    <div className="bg-white shadow rounded-lg p-4 flex justify-between items-center">
-      <div>
-        <h2 className="text-lg font-semibold">{patient.name}</h2>
-        <p className="text-lg font-semibold">
-          {patient.date} | {patient.time}
-        </p>
+    <div className="bg-white hover:shadow-lg rounded-xl p-5 flex flex-col justify-between">
+      <div className="flex items-center gap-3">
+        <img src={appt.patientphoto} alt={appt.patientname} className="w-12 h-12 rounded-full border" />
+        <div>
+          <div>{appt.patientname}</div>
+          <div>{appt.date} • {appt.time}</div>
+        </div>
       </div>
-      <p className="text-lg font-semibold"> {patient.type}</p>
-      <button
-        onClick={() => onViewDetails(patient)}
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-200"
-      >
-        View Details
-      </button>
+      <div className="mt-3 text-sm text-blue-500">{appt.type}</div>
+      <div className={`mt-2 px-3 py-1 rounded-full text-sm w-fit ${
+        appt.status==="Scheduled" ? "bg-yellow-100 text-yellow-700":
+        appt.status==="Completed" ? "bg-green-100 text-green-700" : "bg-red-100 text-red-700"       }`}>
+        {appt.status}
+      </div>
+
+      <div className="mt-4 flex gap-2">
+        {appt.type === "Video Consultaion" && (
+          <button className="flex-1 bg-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-600 flex items-center justify-center gap-1">
+            <Video size={16}/> Start VC
+          </button>
+        )}
+          <button
+            onClick={onViewDetails}
+            className="flex-1 bg-gray-200 text-gray-700 px-3 py-2 rounded-lg hover:bg-gray-300 flex items-center justify-center gap-1"
+          >
+            <FileText size={16}/> Details
+          </button>
+      </div>
     </div>
   );
 };
