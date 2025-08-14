e968e928bce9fe9c76a053ba431855e0330b3d06
Suhan3617 <suhanmakhare@gmail.com>
2025-08-14 13:03:47 +0530

    Refractor Doctor Action Buttons

--- src/components/Doctor/doctoractionbuttons.jsx
+++ src/components/Doctor/doctoractionbuttons.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { ClipboardList, FileText, Video } from "lucide-react";
+import React from "react";
+
+const actions = [
+  {
+    label: "Start Consultation",
+    icon: <Video size={18} />,
+    color: "bg-blue-500 hover:bg-blue-600",
+  },
+  {
+    label: "Write Prescription",
+    icon: <FileText size={18} />,
+    color: "bg-blue-500 hover:bg-blue-600",
+  },
+  {
+    label: "View Medical Records",
+    icon: <ClipboardList size={18} />,
+    color: "bg-blue-500 hover:bg-blue-600",
+  },
+];
 
 const doctoractionbuttons = () => {
   return (
-    <div>
-      
+    <div className="bg-white p-6 rounded-xl shadow flex flex-wrap gap-4">
+      {actions.map((a) => (
+        <button
+          className={`flex items-center gap-2 text-white px-4 py-2 rounded-lg transition ${a.color}`}
+        >
+          {a.icon}
+          {a.label}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default doctoractionbuttons
+export default doctoractionbuttons;
