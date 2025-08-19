55a9b77bdfba8f41dcd7fa3d7f0dd3ca4016ccd7
Suhan3617 <suhanmakhare@gmail.com>
2025-08-19 22:54:55 +0530

    Refractor the Doctor Report Card

--- src/components/Doctor/doctorlabrecords.jsx
+++ src/components/Doctor/doctorlabrecords.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const doctorlabrecords = ({ record }) => {
   return (
-    <div className="bg-white p-4 rounded-xl shadow hover:shadow-2xl transition flex items-center gap-4">
+    <div className="bg-white p-4 rounded-xl shadow hover:shadow-2xl transition flex items-center gap-4 mx-4">
       <div>
         <div className="font-semibold">{record.title}</div>
         <div className="text-sm text-gray-500">
           {record.patientName} • {record.date} • {record.type}
         </div>
       </div>
-      <div>
+      <div className="flex flex-row gap-4">
         <button className="flex-1 bg-gray-200 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-300 flex items-center justify-center gap-1">View</button>
         <button className="px-3 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600">Download</button>
       </div>
