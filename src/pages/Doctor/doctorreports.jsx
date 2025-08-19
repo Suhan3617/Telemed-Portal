0bff4049b7859c30f1e1ebac5ab05c43ed0b2984
Suhan3617 <suhanmakhare@gmail.com>
2025-08-19 15:19:20 +0530

    Refractor the Doctor Reports Page

--- src/pages/Doctor/doctorreports.jsx
+++ src/pages/Doctor/doctorreports.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useState , useMemo } from 'react'
+import Doctorlabrecords from '../../components/Doctor/doctorlabrecords'
+import FiltersBar from '../../components/Doctor/doctorfilterbar'
+import SearchInput from '../../components/Common/searchinput'
+import { records } from '../../data/doctor/mockdata'
 
 const doctorreports = () => {
+
+  const [q, setq] = useState("");
+  const [type, settype] = useState("All");
+
+  const list = useMemo(()=>{
+    return records.filter(r=>{
+      const matchQ = (r.title + r.patientName).toLowerCase().includes(q.toLowerCase());
+      const matchType = type==="All" || r.type===type;
+      return matchQ && matchType;
+    })
+  } , [q.type])
+
   return (
     <div>
       
